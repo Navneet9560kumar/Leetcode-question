4948645dd3a1ffa5706dd45fcd55a8f2bbd0afc6
Navnee <142996178+Navneet9560kumar@users.noreply.github.com>
2024-10-24 22:01:05 +0530

    Time: 4 ms (86.04%), Space: 68.3 MB (45.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,7 +12,7 @@ class Solution {
      public ListNode reverseList(ListNode head) {
         ListNode curr = head;
         ListNode prev = null;
-        ListNode next = head;  // Fixed variable name to 'next' (lowercase)
+        ListNode next = head;  
         
         while (curr != null) {
             next = curr.next;
@@ -24,32 +24,19 @@ class Solution {
     }
 
     public boolean isPalindrome(ListNode head) {
-        if (head.next == null) return true;
-
-        // Creating a new copy of the original list
-        ListNode newHead = new ListNode(head.val);
-        ListNode t1 = head.next;
-        ListNode t2 = newHead;
-        
-        while (t1 != null) {
-            ListNode temp = new ListNode(t1.val);
-            t2.next = temp;
-            t2 = t2.next;
-            t1 = t1.next;
+        ListNode slow = head;
+       ListNode fast = head;
+       while(fast!=null && fast.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+       }
+       ListNode j = reverseList(slow);
+        ListNode i = head;
+        while(j!=null){
+            if(i.val!=j.val)return false;
+            i= i.next;
+            j = j.next;
         }
-
-        // Reverse the copied list
-        newHead = reverseList(newHead);
-
-        // Compare the original list and the reversed copy
-        t1 = head;
-        t2 = newHead;
-        while (t1 != null) {
-            if (t1.val != t2.val) return false;
-            t2 = t2.next;
-            t1 = t1.next;
-        }
-        
         return true;
     }
 }
