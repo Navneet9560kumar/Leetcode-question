663b0d9706f85a638595a20f556362b14d385312
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2024-11-02 01:31:20 +0530

    Time: 0 ms (100%), Space: 45.4 MB (67.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,42 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        int m= arr.length,n=arr[0].length;
-        int[][]helper  = new int [m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                helper[i][j] = arr[i][j];
+        int m = arr.length, n = arr[0].length;
+        // int[][] helper  = new int[m][n]; helper ke jagha mai ek kaam karunga boolean ka arr banate hain for row
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n]; // corrected from boolean[] col = new boolean[m]; to boolean[] col = new boolean[n];
+
+        // First pass to mark the rows and columns that should be set to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // if in case aase dekha ke arr[i][j] = 0, so ham row[i] = true, or col[j] = true; is per ham marking kar rahe perticular row and col per
+                if (arr[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-              if(helper[i][j]==0){
-                 // set i th  raw and jth col  to 0 in arr
-                 for(int b=0;b<n;b++){
-                    arr[i][b]=0;
-                 }
-                 for(int a=0;a<m;a++){
-                    arr[a][j]=0;
-                 }
-              }
+
+        // Set the true row to 0
+        for (int i = 0; i < m; i++) {
+            if (row[i]) { // set ith row is arrya ke puri row ko zero set karna hai
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = 0;
+                }
             }
         }
-        
+
+        // Set the true column to 0
+        for (int j = 0; j < n; j++) {
+            if (col[j]) { // set ith col is arrya ke puri column ko zero set karna hai
+                for (int i = 0; i < m; i++) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
     }
 }
+
+// iske leye we use a hamko (om complecity cahiye per vo nahi hai ham isame ek extra 2D array use kar rahe hai jada space kharch ho raha hai)
+
+// the optimal approach 
+// 1 we make a two-step kon se aase row hai pure ke pure zero ho jaye or kon se aase column hai puri tarha se zero ho jaye
