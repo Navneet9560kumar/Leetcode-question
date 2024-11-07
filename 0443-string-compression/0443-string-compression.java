97f3be1296ad0bc0b9b9e9258bb6e11821fe87a6
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2024-11-07 16:27:31 +0530

    Time: 2 ms (29.12%), Space: 43.8 MB (79.05%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,22 +1,23 @@
 class Solution {
     public int compress(char[] arr) {
-                      String ans = "";
+        StringBuilder ans = new StringBuilder("") ;
         int i = 0, j = 0;
         
         while (j < arr.length) {
             if (arr[j] == arr[i]) {
                 j++;
             } else {
-                ans += arr[i];
+                ans.append(arr[i]);
                 int len = j - i;
-                if (len > 1) ans += len;
+                if (len > 1) ans.append(len);
                 i = j;
             }
         }
         
-        ans += arr[i];
-        int len = j - i;
-        if (len > 1) ans += len;
+                        ans.append(arr[i]);
+                int len = j - i;
+                if (len > 1) ans.append(len);
+
 
         for (i = 0; i < ans.length(); i++) {
             arr[i] = ans.charAt(i);
