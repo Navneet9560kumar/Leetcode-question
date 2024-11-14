d905ff63bf1a3b696f83d1fd833bd6104f5edf9a
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2024-11-14 22:01:35 +0530

    Time: 41 ms (80.36%), Space: 56.7 MB (44.98%) - LeetHub

--- 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,24 +1,27 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-       int left = 1;
-        int right = Integer.MAX_VALUE;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+          int low = 1, high = Integer.MAX_VALUE;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
             if (canDistribute(quantities, n, mid)) {
-                right = mid;
+                high = mid;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
-        return left;
+        return low;
     }
 
-    private boolean canDistribute(int[] quantities, int stores, int maxProductsPerStore) {
-        int requiredStores = 0;
+    private boolean canDistribute(int[] quantities, int stores, int maxProducts) {
+        int neededStores = 0;
         for (int quantity : quantities) {
-            requiredStores += (quantity + maxProductsPerStore - 1) / maxProductsPerStore; 
+            neededStores += (quantity + maxProducts - 1) / maxProducts;
         }
-        return requiredStores <= stores;
+        return neededStores <= stores;
     }
 }
+
+
+
+
+
