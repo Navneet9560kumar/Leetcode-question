ee02c18901c94cd75d53c4161d7763859db57d37
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2024-11-25 23:52:26 +0530

    Time: 1 ms (86.51%), Space: 40.8 MB (57.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-          return (int)(Math.sqrt(x));
+       
+        //   return (int)(Math.sqrt(x));// ye ho jata hai per isko use nahi karege ham or to or new method se karege aaram se (Newton raphson method)
+          long lo=0,hi=(long)x;
+          while(lo<=hi){
+            long mid = lo+(hi-lo)/2;
+             if(mid*mid==(long)x) return (int)mid;
+             else if(mid*mid>(long)x){
+                hi=mid-1;
+             }else if(mid*mid<x){
+                lo= mid+1;
+             }
+          }
+        return (int)hi;
     }
 }
