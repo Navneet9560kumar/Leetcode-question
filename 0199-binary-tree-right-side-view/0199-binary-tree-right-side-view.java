9cb17c08b794f475b81be3fd4631101ab5af9c95
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-02-11 23:22:55 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.59%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,5 +1,5 @@
 /**
- * Definition for a binary tree TreeNode.
+ * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -14,25 +14,27 @@
  * }
  */
 class Solution {
-   public int levels(TreeNode root) {
+
+
+     public int levels(TreeNode root) {
         if (root == null) return 0;
         return 1 + Math.max(levels(root.left), levels(root.right));
     }
 
-    public static void preorder(TreeNode root, int level, List<Integer> ans) {
+    public void perorder(TreeNode root, int level, List<Integer> ans) {
         if (root == null) return;
         
-        if (ans.size() == level) {
-            ans.add(root.val);
+        if (level == ans.size()) {
+            ans.add(root.val);  // Correct way to add elements at new level
         }
-        
-        preorder(root.right, level + 1, ans);
-        preorder(root.left, level + 1, ans);
+
+        perorder(root.right, level + 1, ans); // First right, then left for right-side view
+        perorder(root.left, level + 1, ans);
     }
 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorder(root, 0, ans);
+        perorder(root, 0, ans);
         return ans;
     }
 }
