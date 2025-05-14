e7a2421153a814e16b7dbb8fe06bd579d8ff7f12
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-05-15 01:15:57 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
     
-     double result = Math.pow(x, n);
+    //  double result = Math.pow(x, n);
+    //     return result;
+
+    // without math.pow;
+ long exp = n;
+         if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+          double result = 1.0;
+     while (exp > 0) {
+            if (exp % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+
         return result;
     }
 }
