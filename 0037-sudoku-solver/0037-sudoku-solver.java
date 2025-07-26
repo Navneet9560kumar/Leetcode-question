629f88957437a30d39ae9ce3f2b3a7fa79ac1427
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-07-26 12:40:53 +0530

    Time: 373 ms (26.9%), Space: 41 MB (71.73%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,44 @@
+class Solution {
+     public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) { // use board[0].length for columns
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.'; // backtrack
+                            }
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // all cells are filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+
+            // Check column
+            if (board[i][col] == c) return false;
+
+            // Check 3x3 subgrid
+            int subRow = 3 * (row / 3) + i / 3;
+            int subCol = 3 * (col / 3) + i % 3;
+            if (board[subRow][subCol] == c) return false;
+        }
+        return true;
+    }
+
+}
