c93a4a9b6d1e46cf1c6e3e8543ee55a0f2b04ffe
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-07-29 14:27:18 +0530

    Time: 266 ms (5.31%), Space: 55.1 MB (58.18%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,21 +1,47 @@
 class Solution {
-   public int numberOfSubarrays(int[] nums, int k) {
-        int count = 0; 
-        int oddCount = 0;
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); 
+    public int numberOfSubarrays(int[] arr, int k) {
+        int i = 0, j = 0, a = 0, b = 0, n = arr.length, k2 = 0;
+        int count = 0;
 
-        for (int num : nums) {
-       
-            if (num % 2 != 0) {
-                oddCount++;
-            }
-                if (prefixMap.containsKey(oddCount - k)) {
-                count += prefixMap.get(oddCount - k);
-            }
+        // Move i to first odd number
+        while (i < n && arr[i] % 2 == 0) i++;
 
-          
-            prefixMap.put(oddCount, prefixMap.getOrDefault(oddCount, 0) + 1);
+        // Move j to a window containing first k odd numbers
+        while (j < n && k2 < k) {
+            if (arr[j++] % 2 != 0) k2++;
+        }
+
+        // If we couldn't find k odd numbers, return 0
+        if (k2 < k) return 0;
+
+        j--; // Move back to the last odd
+        b = j + 1;
+
+        // Move b forward to include all trailing even numbers
+        while (b < n && arr[b] % 2 == 0) b++;
+        b--; // b is now last index of that window
+
+        while (b < n) {
+            count += (i - a + 1) * (b - j + 1);
+
+            // Move i to next odd
+            a = i + 1;
+            i++;
+            while (i < n && arr[i] % 2 == 0) i++;
+
+            // Move j to next odd (past current j)
+            j = b + 1;
+            k2 = 0;
+            while (j < n && k2 < 1) {
+                if (arr[j++] % 2 != 0) k2++;
+            }
+            if (k2 < 1) break;
+            j--;
+
+            // Move b forward to include all trailing evens after new j
+            b = j + 1;
+            while (b < n && arr[b] % 2 == 0) b++;
+            b--;
         }
 
         return count;
