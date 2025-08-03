e3679950a7c5eada624d50ade361405317d1764b
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-08-03 13:55:48 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.92%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public List<List<Integer>> arr;
-
-    public void helper(int i, int[] nums, ArrayList<Integer> ans) {
-        if (i == nums.length) {
-            arr.add(new ArrayList<>(ans));
-            return;
-        }
-
-        helper(i + 1, nums, ans);
-        ans.add(nums[i]);
-        helper(i + 1, nums, ans);
-        ans.remove(ans.size() - 1);
-    }
-
-    public List<List<Integer>> subsets(int[] nums) {
-        arr = new ArrayList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        helper(0, nums, ans);
-        return arr;
+    public static List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            int n = nums.length;
+            int total = 1<<n; // 2^n subsets
+            for (int i = 0; i < total; i++) {
+                  List<Integer> list = new ArrayList<>();
+                  for(int  bitIdx=0;bitIdx<n; bitIdx++){
+                        int mask = (1 << bitIdx);
+                        if((i & mask) != 0){
+                              list.add(nums[bitIdx]);
+                        }
+                  }
+                  ans.add(list);
+            }
+            return ans;
     }
 }
