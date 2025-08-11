033b0b690e59ba93bbda41f75eb3183b7a63ad9d
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-08-11 13:35:03 +0530

    Time: 1 ms (99.85%), Space: 46.3 MB (46.31%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,9 @@
 class Solution {
-   public int singleNumber(int[] nums) {
-        int n = nums.length;
-        
-        int maxi = Math.abs(nums[0]);
-        for (int i = 1; i < n; i++) {
-            maxi = Math.max(maxi, Math.abs(nums[i]));
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
         }
-        
-        int[] hash = new int[2 * maxi + 1];
-        
-        for (int i = 0; i < n; i++) {
-            hash[nums[i] + maxi]++;
-        }
-        
-       
-        for (int i = 0; i < n; i++) {
-            if (hash[nums[i] + maxi] == 1) {
-                return nums[i];
-            }
-        }
-        
-        return -1; 
+        return res;
     }
 }
