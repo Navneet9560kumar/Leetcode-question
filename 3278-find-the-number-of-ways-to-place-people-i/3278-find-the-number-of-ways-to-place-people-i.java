0d8af16c28fa01fca6241dd8bc2cc20c2070d5c1
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-09-03 00:15:23 +0530

    Time: 4 ms (100%), Space: 45 MB (14.93%) - LeetHub

---
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        // Step 1: Sort points by x ascending, then by y descending
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1];
+            return a[0] - b[0];
+        });
+
+        int pairCount = 0;
+        int n = points.length;
+
+        // Step 2: Iterate through all points as potential "upper-left" points
+        for (int i = 0; i < n; i++) {
+            int upperY = points[i][1];
+            int lowerYLimit = Integer.MIN_VALUE;
+
+            // Step 3: Check subsequent points as potential "bottom-right" points
+            for (int j = i + 1; j < n; j++) {
+                int currentY = points[j][1];
+                if (currentY <= upperY && currentY > lowerYLimit) {
+                    pairCount++;
+                    lowerYLimit = currentY;
+                    if (currentY == upperY) break;
+                }
+            }
+        }
+        return pairCount;
+    }
+}
