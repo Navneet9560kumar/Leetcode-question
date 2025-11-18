609802827be047afa50775f2c4e0d1eb5f4d4a00
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-11-18 23:05:22 +0530

    Time: 10 ms (14.12%), Space: 46.8 MB (17.92%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.java
@@ -1,34 +1,23 @@
-import java.util.Arrays;
-
 class Solution {
-    public int[] answerQueries(int[] arr, int[] queries) {
-        Arrays.sort(arr); // Sort array for prefix sum and binary search
-
-        int n = arr.length;
-
-        // Create prefix sum array in-place
-        for (int i = 1; i < n; i++) {
-            arr[i] += arr[i - 1];
-        }
-
-        int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            int lo = 0, hi = n - 1, best = 0;
-
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (arr[mid] <= queries[i]) {
-                    best = mid + 1; // +1 because we want the count
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 1;
-                }
+   public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+            int n = nums.length;
+            for(int i=1;i<n;i++){
+                  nums[i] += nums[i-1];
             }
-
-            ans[i] = best;
-        }
-
-        return ans;
-    }
+            int[] ans = new int[queries.length];
+            for(int i=0;i<queries.length;i++){
+                  int lo = 0, hi = n-1;
+                  while(lo<=hi){
+                        int mid = lo + (hi - lo) / 2;
+                        if(nums[mid]>queries[i])hi  =mid-1;
+                        else{
+                              ans[i]  = Math.min(queries[i], mid+1);
+                              lo = mid + 1;
+                        }
+                  }
+            
+            }
+            return ans;
+            }
 }
