b33d643e8ccd112fe65a304deb3de33c4969e0b3
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-11-19 23:58:42 +0530

    Time: 20 ms (97.13%), Space: 48.4 MB (24.45%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int subarraySum(int[] arr, int k) {
-      int n = arr.length;
-        Map<Integer, Integer> mpp = new HashMap<>();
-        int preSum = 0, cnt = 0;
+public int subarraySum(int[] arr, int k){
+        //     int count = 0;
+        //     for(int i=0;i<arr.length;i++){
+        //           int sum = 0;
+        //           for(int j=i;j<arr.length;j++){
+        //                 sum += arr[j];
+        //                 if(sum == k)count++;
+        //           }
+        //     }
+        //     return count;
+        //   }   method frist 
+ int count =0, n =arr.length;
+            // make arr prefix sum array
+            for(int i=1;i<n;i++){
+                  arr[i] += arr[i-1];
 
-        mpp.put(0, 1);
-        for (int i = 0; i < n; i++) {
-            preSum += arr[i];
+            }
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i=0;i<n;i++){
+                  int ele = arr[i];
+                  if(ele==k)count++;
+                  int rem = ele -k;
+                  if(map.containsKey(rem)) count += map.get(rem);
+                  if(map.containsKey(ele)){
+                        int freq = map.get(ele);
+                        map.put(ele, freq +1);
 
-            int remove = preSum - k;
-            cnt += mpp.getOrDefault(remove, 0);
-
-            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
-        }
-        return cnt;
-
-             
-    }
+                  }else map.put(ele, 1);
+            }
+            return count;
+}
+        
 }
