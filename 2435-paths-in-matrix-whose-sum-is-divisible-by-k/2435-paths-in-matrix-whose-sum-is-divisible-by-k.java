1b50fd811b78691eb20b71066784b45fd55a308c
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-11-26 23:26:24 +0530

    Time: 53 ms (78.83%), Space: 101.3 MB (16.66%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,38 +1,59 @@
 class Solution {
 
       int M = 1_000_000_007;
-    Integer[][][] t; 
-    int solve( int row, int col, int remain, int[][] grid, int k){
-        int m = grid.length;
-        int n =  grid[0].length;
+//     Integer[][][] t; 
+//     int solve( int row, int col, int remain, int[][] grid, int k){
+//         int m = grid.length;
+//         int n =  grid[0].length;
 
-        if(row>=m || col >=n) return 0;
+//         if(row>=m || col >=n) return 0;
 
-        // base hai ye 
-        if(row ==m-1 && col ==n-1){
-            return ((remain + grid[row][col]) %k==0) ? 1:0;
-        }
+//         // base hai ye 
+//         if(row ==m-1 && col ==n-1){
+//             return ((remain + grid[row][col]) %k==0) ? 1:0;
+//         }
 
-        if(t[row][col][remain]!= null) return t[row][col][remain];
+//         if(t[row][col][remain]!= null) return t[row][col][remain];
 
-        int newRemain = (remain + grid[row][col])%k;
+//         int newRemain = (remain + grid[row][col])%k;
 
-        int down = solve(row +1, col, newRemain, grid, k);
-        int right = solve(row,col+1, newRemain, grid, k);
+//         int down = solve(row +1, col, newRemain, grid, k);
+//         int right = solve(row,col+1, newRemain, grid, k);
 
-        return t[row][col][remain] = (int)(((long)down+ right)%M);
+//         return t[row][col][remain] = (int)(((long)down+ right)%M);
 
+// }
 
-
-
-
-    }
+// this iis method number is one with the help of a //Approach-1 (Recursion + Memoization : DP On Grids)
+//T.C : O(m*n*k)
+//S.C : O(m*n*k)
     public int numberOfPaths(int[][] grid, int k) {
-int m = grid.length;
+        int m =  grid.length;
         int n = grid[0].length;
 
-        t = new Integer[m][n][k];
+        int[][][] t = new int[m+1][n+1][k+1];
+        // base case 
+        for(int remain =0; remain<k; remain++){
+            t[m-1][n-1][remain]= ((remain+grid[m-1][n-1])% k==0)? 1: 0;
+        }
+         // Fill bottom-up
+          for (int row = m - 1; row >= 0; row--) {
+            for (int col = n - 1; col >= 0; col--) {
 
-        return solve(0, 0, 0, grid, k);
+                for (int remain = 0; remain < k; remain++) {
+
+                    if (row == m - 1 && col == n - 1)
+                        continue;
+
+                    int newRemain = (remain + grid[row][col]) % k;
+
+                    long down  = t[row + 1][col][newRemain];
+                    long right = t[row][col + 1][newRemain];
+
+                    t[row][col][remain] = (int)((down + right) % M);
+                }
+            }
+        }
+        return t[0][0][0];
     }
 }
