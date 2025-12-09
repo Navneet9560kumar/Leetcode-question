bb9233e52a5a3c73a1d02e5568e5ff55aaf320a8
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-12-09 21:33:15 +0530

    Time: 169 ms (82.17%), Space: 185.8 MB (47.29%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.java
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -1,26 +1,28 @@
 class Solution {
        static final int M = (int)1e9 + 7;
+
     public int specialTriplets(int[] nums) {
-          Map<Integer, Integer> mpLeft = new HashMap<>();
-        Map<Integer, Integer> mpRight = new HashMap<>();
+        Map<Integer, Integer> validI = new HashMap<>();
+        Map<Integer, Integer> validJ = new HashMap<>();
 
-        int res =0;
+        int result = 0;
 
-        for(int num: nums){
-           mpRight.put(num, mpRight.getOrDefault(num, 0) + 1);
+        for (int num : nums) {
+
+            // If num is valid k (k must be even)
+            if (num % 2 == 0) {
+                result = (result + validJ.getOrDefault(num / 2, 0)) % M;
+            }
+
+            // Is current num a valid j?
+            int updatedJ = (validJ.getOrDefault(num, 0) +
+                            validI.getOrDefault(num * 2, 0)) % M;
+            validJ.put(num, updatedJ);
+
+            // Count num as a valid i
+            validI.put(num, validI.getOrDefault(num, 0) + 1);
         }
-        // Travessle
-        for(int num: nums){
-             mpRight.put(num, mpRight.get(num) - 1);
 
-            int left  = mpLeft.getOrDefault(num * 2, 0);
-            int right = mpRight.getOrDefault(num * 2, 0);
-
-            long add = (1L * left * right) % M;
-            res = (int)((res + add) % M);
-
-            mpLeft.put(num, mpLeft.getOrDefault(num, 0) + 1);
-        }
-        return res;
+        return result;
     }
 }
