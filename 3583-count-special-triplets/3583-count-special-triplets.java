e384b81afbdfbb2987b06514ac95a469104f56c2
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2025-12-09 21:03:34 +0530

    Time: 261 ms (30.23%), Space: 190.2 MB (38.76%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -0,0 +1,26 @@
+class Solution {
+       static final int M = (int)1e9 + 7;
+    public int specialTriplets(int[] nums) {
+          Map<Integer, Integer> mpLeft = new HashMap<>();
+        Map<Integer, Integer> mpRight = new HashMap<>();
+
+        int res =0;
+
+        for(int num: nums){
+           mpRight.put(num, mpRight.getOrDefault(num, 0) + 1);
+        }
+        // Travessle
+        for(int num: nums){
+             mpRight.put(num, mpRight.get(num) - 1);
+
+            int left  = mpLeft.getOrDefault(num * 2, 0);
+            int right = mpRight.getOrDefault(num * 2, 0);
+
+            long add = (1L * left * right) % M;
+            res = (int)((res + add) % M);
+
+            mpLeft.put(num, mpLeft.getOrDefault(num, 0) + 1);
+        }
+        return res;
+    }
+}
