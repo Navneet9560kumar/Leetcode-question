dbb78b37ba82c31490e6c00b52b3c70c08698d91
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2026-02-07 01:29:30 +0530

    Time: 1 ms (99.26%), Space: 46.9 MB (15.24%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -1,10 +1,20 @@
 class Solution {
-    public int[] constructTransformedArray(int[] A) {
-        int n = A.length;
+    public int[] constructTransformedArray(int[] nums) {
+
+        int n = nums.length;
         int[] res = new int[n];
 
-        for (int i = 0; i < n; i++)
-            res[i] = A[(((i + A[i]) % n) + n) % n];
+        for (int i = 0; i < n; i++) {
+
+            int jump = nums[i] % n;
+            int newIdx = (i + jump) % n;
+
+            if (newIdx < 0) {
+                newIdx += n;
+            }
+
+            res[i] = nums[newIdx];
+        }
 
         return res;
     }
