632257d3cb3ea0fb18325d745d3d5697e4ffbdf0
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2026-02-07 23:46:45 +0530

    Time: 115 ms (9.21%), Space: 48.1 MB (9.21%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minimumDeletions(String s) {
-    if (s == null || s.isEmpty()) return 0;
+        int n = s.length();
+        int count =0;
+        Stack<Character> st = new Stack<>();
 
-        int countB = 0; //keep a count of Bs
-        int removals = 0; //keep a count of removed As
-
-        for (int i = 0; i < s.length(); i++) {
-
-            if (s.charAt(i) == 'a') {
-                if (countB > 0) { // only if there are Bs before this A
-                    ++removals; // remove this A
-                    --countB; // and decrement the Bs count
-                }
-            } else {
-                ++countB; // keep incrementing the Bs count
+        for(int i=0;i<n;i++){
+            if(!st.isEmpty()&& s.charAt(i)=='a' && st.peek()=='b'){
+                st.pop();
+                count++;
+            }else{
+                st.push(s.charAt(i));
             }
         }
-       return removals;
-}
+        return count;
+    }
 }
