bec36d3af62f006e62bf6a98244cdf8edeed1aed
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2026-02-25 22:15:01 +0530

    Time: 1 ms (99.12%), Space: 43.1 MB (15.43%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,17 +1,19 @@
 class Solution {
-     public int lastStoneWeight(int[] arr) {
-          PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int ele: arr ){
-            pq.add(ele);
-        }
-
-        while (pq.size()>1) {
-            int y = pq.remove();
-            int x =pq.remove();
-            if(y!=x)pq.add(y-x);
-        }
-        if(pq.size()==0)return 0;
-        else return pq.remove();
+    public int lastStoneWeight(int[] arr) {
+          int n=  arr.length;
+            ArrayList<Integer>st = new ArrayList<>();
+            PriorityQueue<Integer> pq =new PriorityQueue<>(Collections.reverseOrder());
+            for(int ele: arr){
+                pq.add(ele);
+            }
+            while(pq.size()>1){
+              int y = pq.poll();
+              int x = pq.poll();
+                    if (y != x) {
+                pq.add(y - x);
+            }
+            }
+          return pq.isEmpty() ? 0 : pq.poll();
 
     }
 }
