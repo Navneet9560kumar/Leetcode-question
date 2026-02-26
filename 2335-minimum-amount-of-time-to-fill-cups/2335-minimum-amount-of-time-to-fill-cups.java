66deca2149ad5b87dff002b6c147fa2893a59c7b
Code rush <142996178+Navneet9560kumar@users.noreply.github.com>
2026-02-26 23:47:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (41.42%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int fillCups(int[] amount) {
+
+        int total = amount[0] + amount[1] + amount[2];
+        int max = Math.max(amount[0], 
+                  Math.max(amount[1], amount[2]));
+
+        return Math.max(max, (total + 1) / 2);
+    }
+}
