217c4490dfe7e8093e24a65b6874083b0f663d56
Code rush <1611navneetkumar@gmail.com>
2026-04-19 02:32:08 +0530

    Time: 1 ms (100%), Space: 119.4 MB (23.93%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,38 +10,32 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int count = 0;
-        ListNode front = null;
-        ListNode end = null;
-        ListNode curr = head;
-
-        // Traverse the list to find the k-th node from the start
-        while (curr != null) {
-            count++;
-
-            // Set the end pointer to the head after we reach the k-th node
-            if (count == k) {
-                front = curr;
-                end = head;
-            }
-
-            // Move the end pointer to find the k-th node from the end
-            if (count > k) {
-                end = end.next;
-            }
-
-            curr = curr.next;
+        ListNode fast = head;
+        ListNode slow = head;
+        // Step 1: move fast to k-th node
+        for (int i = 1; i < k; i++) {
+            fast = fast.next;
         }
 
-        // Swap the values of the front and end nodes
-        swapValues(front, end);
+        ListNode first = fast;// k-th from start
+
+        // Step 2: move both until fast reaches end
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        ListNode second = slow; // k-th from end
+
+      
+
+       // Step 3: swap values
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
 
         return head;
-    }
 
-    private void swapValues(ListNode node1, ListNode node2) {
-        int temp = node1.val;
-        node1.val = node2.val;
-        node2.val = temp;
     }
 }
