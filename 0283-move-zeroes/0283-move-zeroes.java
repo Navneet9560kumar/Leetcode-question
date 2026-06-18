5740a3e011a636350d0778e95d062a7723621a1c
Code rush <1611navneetkumar@gmail.com>
2026-06-18 18:12:29 +0530

    Time: 2 ms (91.95%), Space: 47.6 MB (75.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,16 @@
 class Solution {
-      public void moveZeroes(int[] arr) {
-           int noz =0;
-            int n = arr.length;
-            for(int ele : arr){
-                  if(ele==0) noz++;
+    public static void moveZeroes(int[] arr) {
 
-            }
-            for(int  x =0; x<noz; x++){
-                  for (int i = 0; i < n-1-x; i++) {
-                        if(arr[i]==0){
-                              int temp = arr[i];
-                              arr[i] = arr[i+1];
-                              arr[i+1] =temp;
-                  }
-                  }
-                  
-            }
-      }
+    int j = 0;
+
+    // Non-zero elements ko front me lao
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] != 0) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+    }
+}
 }
