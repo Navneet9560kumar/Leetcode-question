151869a0a9f59defc331b026e082d784195dcd19
Code rush <1611navneetkumar@gmail.com>
2026-08-08 23:47:21 +0530

    Time: 0 ms (100%), Space: 48.1 MB (52.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,54 +1,38 @@
 class Solution {
+
+    public static int findLeftOrRightMost(int[] nums, int t, int flag) {
+        int n = nums.length;
+        int ans = -1;
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > t) {
+                r = mid - 1;
+            } else if (nums[mid] < t) {
+                l = mid + 1;
+            } else {
+                ans = mid;
+
+                if (flag == 0) {
+                    // Leftmost
+                    r = mid - 1;
+                } else {
+                    // Rightmost
+                    l = mid + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-
-        return new int[]{first, last};
-    }
-
-    private int findFirst(int[] nums, int target) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        int ans = -1;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                hi = mid - 1;   // aur left jao
-            } 
-            else if (nums[mid] < target) {
-                lo = mid + 1;
-            } 
-            else {
-                hi = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    private int findLast(int[] nums, int target) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        int ans = -1;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                lo = mid + 1;   // aur right jao
-            } 
-            else if (nums[mid] < target) {
-                lo = mid + 1;
-            } 
-            else {
-                hi = mid - 1;
-            }
-        }
-
-        return ans;
+        return new int[]{
+            findLeftOrRightMost(nums, target, 0),
+            findLeftOrRightMost(nums, target, 1)
+        };
     }
 }
